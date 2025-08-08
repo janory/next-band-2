ce9e46d54f34ac9273dc936d7da34b0737506e33
János Szathmáry <29057504+janory@users.noreply.github.com>
2025-08-08 16:35:22 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly!!!!!!!!!!!!!aaa
+            Save and see your changes blazing fast.
           </li>
         </ol>
 
