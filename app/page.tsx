da4eac883f7431922fd4559b8b50010db169ca93
János Szathmáry <29057504+janory@users.noreply.github.com>
2025-08-08 18:39:54 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Done faster.
+            GG
           </li>
         </ol>
 
